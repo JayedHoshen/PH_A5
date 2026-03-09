4f1be58d3bd611e6172dad46e5bd1f150d21be28
jayedHoshen <jayedkn64@gmail.com>
2026-03-09 09:14:18 +0600

    implement login functionality

--- scripts/login.js
+++ scripts/login.js
@@ -1,1 +1,17 @@
-console.log("Login page script is running...");
+document.getElementById("btn-login").addEventListener("click", (event) => {
+  const inputUsername = document.getElementById("input-username");
+  const inputPassword = document.getElementById("input-password");
+
+  const username = inputUsername.value.trim();
+  const password = inputPassword.value.trim();
+
+  if (username === "admin" && password === "admin123") {
+    alert("Login successful!");
+    window.location.href = "dashboard.html";
+  } else {
+    inputUsername.value = "";
+    inputPassword.value = "";
+    alert("Invalid username or password.");
+    return;
+  }
+});
